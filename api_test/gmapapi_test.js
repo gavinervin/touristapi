0011d699be266db3fe0651a2464372d66ec90d3a
Thomas Ervin <gavinervin47@gmail.com>
2017-10-09 19:48:18 -0400

    repush api

--- api_test/gmapapi_test.js
+++ api_test/gmapapi_test.js
@@ -1,17 +1,26 @@
 
-/*default map on app, coords set to our classroom location*/
-/*maptype set to hybrid instead of default streetmap. this gives satellite view with markers*//
- function initAutocomplete() {
+<input id="test-travel-map" class="controls" type="text" placeholder="Search Box">
+    <div id="map"></div>
+    <script>
+      
+      /*preset map on app, coords set to our classroom location*/
+/*maptype set to hybrid instead of default streetmap. this gives satellite view with markers*/
+
+      function initAutocomplete() {
         var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 35.89989698213954, lng: -79.01249361429444},
           zoom: 19,
           mapTypeId: google.maps.MapTypeId.HYBRID
         });
 
+        /*----------------------------------------------------------------
+        [enter code to display places markers(shops, restaurants, activities)]
 
-         /*input bar for testing, will move from inside the map to our html*/
+        ---------------------------------------------------------------------*/
 
-        var input = document.getElementById('pac-input');
+        /*input bar for testing, will move from inside the map to our html*/
+
+        var input = document.getElementById('test-travel-map');
         var searchBox = new google.maps.places.SearchBox(input);
         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
@@ -34,9 +43,10 @@
           });
           markers = [];
 
-        var bounds = new google.maps.LatLngBounds();
+          var bounds = new google.maps.LatLngBounds();
           places.forEach(function(place) {
             if (!place.geometry) {
+              
               return;
             }
             var icon = {
@@ -55,7 +65,7 @@
             }));
 
              if (place.geometry.viewport) {
-            
+              
               bounds.union(place.geometry.viewport);
             } else {
               bounds.extend(place.geometry.location);
